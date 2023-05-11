2b3d6c5a320e0c0b721abae854b7d521d6c838bc
toriomara <budkevich.vladimir@gmail.com>
2023-05-11 22:54:13 +0300

    check my code for tests_19

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,11 +4,11 @@ const { STATUS_CODES, MESSAGES } = require('../utils/constants');
 const getCards = async (req, res) => {
   try {
     const cards = await Card.find({});
-    res.status(STATUS_CODES['200_OK']).send(cards);
+    res.status(STATUS_CODES.OK).send(cards);
   } catch (err) {
     res
-      .status(STATUS_CODES['500_INTERNAL_SERVER_ERROR'])
-      .send(MESSAGES['500_INTERNAL_SERVER_ERROR']);
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send({ message: MESSAGES.INTERNAL_SERVER_ERROR });
   }
 };
 
@@ -16,16 +16,16 @@ const createCard = async (req, res) => {
   try {
     const { name, link } = req.body;
     const card = await Card.create({ name, link, owner: req.user._id });
-    res.status(STATUS_CODES['200_OK']).send(card);
+    res.status(STATUS_CODES.OK).send(card);
   } catch (err) {
     if (err.name === 'ValidationError') {
       res
-        .status(STATUS_CODES['400_BAD_REQUEST'])
-        .send(`{MESSAGES.['400_BAD_REQUEST']} при создании карточки`);
+        .status(STATUS_CODES.BAD_REQUEST)
+        .send({ message: `${MESSAGES.BAD_REQUEST} при создании карточки` });
     } else {
       res
-        .status(STATUS_CODES['500_INTERNAL_SERVER_ERROR'])
-        .send(MESSAGES['500_INTERNAL_SERVER_ERROR']);
+        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .send({ message: MESSAGES.INTERNAL_SERVER_ERROR });
     }
   }
 };
@@ -36,19 +36,19 @@ const deleteCardById = async (req, res) => {
     const card = await Card.findByIdAndDelete(cardId);
     if (!card) {
       res
-        .status(STATUS_CODES['404_NOT_FOUND'])
-        .send(`Карточка с указанным _id не найдена`);
+        .status(STATUS_CODES.NOT_FOUND)
+        .send({ message: `Карточка с указанным _id не найдена` });
     }
-    res.status(STATUS_CODES['200_OK']).send(card);
+    res.status(STATUS_CODES.OK).send(card);
   } catch (err) {
     if (err.name === 'CastError') {
       res
-        .status(STATUS_CODES['400_BAD_REQUEST'])
-        .send(MESSAGES['400_BAD_REQUEST']);
+        .status(STATUS_CODES.BAD_REQUEST)
+        .send({ message: MESSAGES.BAD_REQUEST });
     } else {
       res
-        .status(STATUS_CODES['500_INTERNAL_SERVER_ERROR'])
-        .send(MESSAGES['500_INTERNAL_SERVER_ERROR']);
+        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .send({ message: MESSAGES.INTERNAL_SERVER_ERROR });
     }
   }
 };
@@ -60,20 +60,20 @@ const likeCard = async (req, res) => {
       { $addToSet: { likes: req.user._id } },
       { new: true }
     ).orFail();
-    res.status(STATUS_CODES['200_OK']).send(cards);
+    res.status(STATUS_CODES.OK).send(cards);
   } catch (err) {
     if (err.name === 'DocumentNotFoundError') {
       res
-        .status(STATUS_CODES['404_NOT_FOUND'])
-        .send(`Передан несуществующий _id карточки`);
+        .status(STATUS_CODES.NOT_FOUND)
+        .send({ message: `Передан несуществующий _id карточки` });
     } else if (err.name === 'CastError') {
       res
-        .status(STATUS_CODES['400_BAD_REQUEST'])
-        .send(`{MESSAGES['400_BAD_REQUEST']} для постановки лайка`);
+        .status(STATUS_CODES.BAD_REQUEST)
+        .send({ message: `${MESSAGES.BAD_REQUEST} для постановки лайка` });
     } else {
       res
-        .status(STATUS_CODES['500_INTERNAL_SERVER_ERROR'])
-        .send(MESSAGES['500_INTERNAL_SERVER_ERROR']);
+        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .send({ message: MESSAGES.INTERNAL_SERVER_ERROR });
     }
   }
 };
@@ -85,20 +85,20 @@ const dislikeCard = async (req, res) => {
       { $pull: { likes: req.user._id } },
       { new: true }
     ).orFail();
-    res.status(STATUS_CODES['200_OK']).send(cards);
+    res.status(STATUS_CODES.OK).send(cards);
   } catch (err) {
     if (err.name === 'DocumentNotFoundError') {
       res
-        .status(STATUS_CODES['404_NOT_FOUND'])
-        .send(`Передан несуществующий _id карточки`);
+        .status(STATUS_CODES.NOT_FOUND)
+        .send({ message: `Передан несуществующий _id карточки` });
     } else if (err.name === 'CastError') {
       res
-        .status(STATUS_CODES['400_BAD_REQUEST'])
-        .send(`{MESSAGES['400_BAD_REQUEST']} для постановки лайка`);
+        .status(STATUS_CODES.BAD_REQUEST)
+        .send({ message: `${MESSAGES.BAD_REQUEST} для постановки лайка` });
     } else {
       res
-        .status(STATUS_CODES['500_INTERNAL_SERVER_ERROR'])
-        .send(MESSAGES['500_INTERNAL_SERVER_ERROR']);
+        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .send({ message: MESSAGES.INTERNAL_SERVER_ERROR });
     }
   }
 };
