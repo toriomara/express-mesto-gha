2b871a2629a8fae5a60b7911fcb15d65929e8676
toriomara <budkevich.vladimir@gmail.com>
2023-05-13 15:44:08 +0300

    check my code for tests_34

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardsRouter = require('./cards');
-const unpathRouter = require('./unpath');
+const { STATUS_CODES, MESSAGES } = require('../utils/constants');
+// const unpathRouter = require('./unpath');
 
 router.use('/users', userRouter);
 router.use('/cards', cardsRouter);
-router.use('/*', unpathRouter);
+// router.use('/*', unpathRouter);
+router.use('/*', (req, res) =>
+  res.status(STATUS_CODES.NOT_FOUND).send({ message: MESSAGES.NOT_FOUND })
+);
+
 module.exports = router;
