2f3b52f99bf1c11541c82ef0196b2b0aeb5a5c3c
toriomara <budkevich.vladimir@gmail.com>
2023-05-22 16:43:47 +0300

    feat: check my code for tests_08

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const cardsRoutes = require('./cards');
 const { STATUS_CODES, MESSAGES } = require('../utils/constants');
 const auth = require('../middlewares/auth');
 
-const { createUser, login } = require('../controllers/users');
+// const { createUser, login } = require('../controllers/users');
+const authRoutes = require('./auth');
 
-router.use('/signup', createUser);
-router.use('/signin', login);
+router.use('/signup', authRoutes);
+router.use('/signin', authRoutes);
 
 router.use('/users', auth, userRoutes);
 router.use('/cards', auth, cardsRoutes);
