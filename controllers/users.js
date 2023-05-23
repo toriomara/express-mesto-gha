154edc0f103f1d7703cd60106b0c9ba3d938da1f
toriomara <budkevich.vladimir@gmail.com>
2023-05-23 17:39:23 +0300

    feat: check my code for tests_17

--- controllers/users.js
+++ controllers/users.js
@@ -36,11 +36,11 @@ const createUser = async (req, res, next) => {
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email }).select('+password');
-    const isValidPassword = await bcrypt.compare(password, user.password);
     if (!email || !password) {
       return next(new UnauthorizedError('Email или пароль не могут быть пустыми'));
     }
+    const user = await User.findOne({ email }).select('+password');
+    const isValidPassword = await bcrypt.compare(password, user.password);
     if (!user || !isValidPassword) {
       return next(new UnauthorizedError(MESSAGES.UNAUTHORIZED));
     }
