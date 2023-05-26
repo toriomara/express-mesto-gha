7437a79ed2ca8610fdc5f4fdd34896c948df8903
toriomara <budkevich.vladimir@gmail.com>
2023-05-26 17:58:08 +0300

    feat: check my code for tests_34

--- utils/validation.js
+++ utils/validation.js
@@ -8,14 +8,14 @@ const validateSignup = celebrate({
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(REGEX_URL),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 });
 
 const validateSignin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 });
 
