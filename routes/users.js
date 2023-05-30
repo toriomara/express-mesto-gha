f7a87740265464948de68b6f4c6d9c2481c0c848
toriomara <budkevich.vladimir@gmail.com>
2023-05-30 23:01:16 +0300

    feat: check my code for tests_66

--- routes/users.js
+++ routes/users.js
@@ -11,10 +11,9 @@ const {
   validateUserId,
   validateUserAvatar,
 } = require('../utils/validation');
-const { auth } = require('../middlewares/auth');
 
-userRouter.get('/', auth, getUsers);
-userRouter.get('/me', auth, getYourself);
+userRouter.get('/', getUsers);
+userRouter.get('/me', getYourself);
 userRouter.get('/:userId', validateUserId, getUserById);
 userRouter.patch('/me', validateUser, updateUser);
 userRouter.patch('/me/avatar', validateUserAvatar, updateAvatar);
