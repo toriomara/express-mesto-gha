75ed9e5e2d73c51c5cb3adf2853a8ed18e818695
toriomara <budkevich.vladimir@gmail.com>
2023-06-05 15:45:09 +0300

    feat: check my code for tests_106

--- controllers/cards.js
+++ controllers/cards.js
@@ -30,7 +30,8 @@ const deleteCardById = (req, res, next) => {
       if (!card) {
         return next(new NotFoundError(MESSAGES.NOT_FOUND));
       }
-      if (!card.owner.equals(req.user._id)) {
+      // if (!card.owner.equals(req.user._id)) {
+      if (card.owner !== req.user._id) {
         return next(new ForbiddebError(MESSAGES.FORBIDDEN));
       }
       return Card
